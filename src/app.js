c9f0dd3d4b725f368b127b33e3e7692b5a02284c
Miquel Ortiz Graupera <sisasfx@gmail.com>
2023-02-28 21:46:11 +0000

    fix: add more complexity

--- src/app.js
+++ src/app.js
@@ -8,18 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   const ul = document.getElementById("ulDomaineName");
-  const h4 = document.getElementById("h4domaineName");
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
+  let extension = [".com", ".es", ".net", ".co", ".mx"];
   function domaineName() {
     pronoun.forEach(pr => {
       adj.forEach(ad => {
         noun.forEach(no => {
-          h4.append(`${pr}${ad}${no}.com `);
-          const li = document.createElement("li");
-          ul.append(li);
-          li.append(`${pr}${ad}${no}.com `);
+          extension.forEach(ex => {
+            const li = document.createElement("li");
+            ul.append(li);
+            li.append(`${pr}${ad}${no}${ex}`);
+          });
         });
       });
     });
